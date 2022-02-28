c857629a4fe625e26d202cc80539d54c2518013c
rubeniriso <rubenirisoe@gmail.com>
2022-02-28 20:10:29 +0100

    Code polished a bit with functions retrieveInputArguments and retrieveStringDelimiter

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -8,24 +8,11 @@ class StringCalculator
         $sum = 0;
         $errorMessage = "";
         $thereAreErrors = false;
+        $standardDelimiter = "/[\n,]/";
         if($number != ""){
-            if (($number[0]) == "/" && ($number[1]) == "/"){
-                $lineJumpPosition = strpos($number, "\n");
-                $customDelimiter = substr($number, 2, $lineJumpPosition-1);
-                $inputArguments = substr($number, $lineJumpPosition+1, (strlen($number)));
-
-                $inputArguments = preg_split("/[$customDelimiter]/", $inputArguments);
-
-                $negativeNumberArray = array_filter($inputArguments, function($x) {
-                    return $x < 0 && $x != "";
-                });
-
-                if (sizeof($negativeNumberArray)>0){
-                    $errorMessage .= "Negative not allowed : " . implode(', ', $negativeNumberArray) . " ";
-                    $thereAreErrors = true;
-                }
-            }
-            else {
+            $delimiter = $this->retrieveStringDelimiter($number);
+            $inputArguments = $this->retrieveInputArguments($number, $delimiter);
+            if ($delimiter == $standardDelimiter){
                 $occurrenceLineJumpComma = strpos($number, ",\n");
                 $occurrenceCommaLineJump = strpos($number, "\n,");
                 if ($occurrenceLineJumpComma != false) {
@@ -35,32 +22,53 @@ class StringCalculator
                     $errorMessage .= "Number expected but '\n' found at position " . $occurrenceCommaLineJump + 1 . " ";
                     $thereAreErrors = true;
                 }
-                $inputArguments = preg_split("/[\n,]/", $number);
-
-                $positionOfLastElement = sizeof($inputArguments) - 1;
-
-                $negativeNumberArray = array_filter($inputArguments, function($x) {
-                    return $x < 0 && $x != "";
-                });
-                if (sizeof($negativeNumberArray)>0){
-                    $errorMessage .= "Negative not allowed : " . implode(', ', $negativeNumberArray) . " ";
-                    $thereAreErrors = true;
-                }
-
-                if ($inputArguments[$positionOfLastElement] == "") {
-                    $errorMessage .= "Number expected but NOT found ";
-                    $thereAreErrors = true;
-
-                }
             }
+
+            $negativeNumberArray = array_filter($inputArguments, function($x) {
+                return $x < 0 && $x != "";
+            });
+            if (sizeof($negativeNumberArray)>0) {
+                $errorMessage .= "Negative not allowed : " . implode(', ', $negativeNumberArray) . " ";
+                $thereAreErrors = true;
+            }
+
+            $positionOfLastElement = sizeof($inputArguments) - 1;
+            if ($inputArguments[$positionOfLastElement] == "") {
+                $errorMessage .= "Number expected but NOT found ";
+                $thereAreErrors = true;
+            }
+
             if ($thereAreErrors){
                 return substr($errorMessage, 0, -1);
             }
-            foreach ($inputArguments as $index => $argument) {
-                $sum = $sum + $argument;
+
+            else {
+                foreach ($inputArguments as $index => $argument) {
+                    $sum = $sum + $argument;
+                }
+                return $sum;
             }
-            return $sum;
         }
         return(0);
+    }
+    private function retrieveStringDelimiter ($number)
+    {
+        if (($number[0]) == "/" && ($number[1]) == "/") {
+            $lineJumpPosition = strpos($number, "\n");
+            $customDelimiter = substr($number, 2, $lineJumpPosition-1);
+            return $customDelimiter;
+        }
+        return "/[\n,]/";
+    }
+    private function retrieveInputArguments ($number, $delimiter){
+        if ($delimiter == "/[\n,]/"){
+            $inputArguments = preg_split($delimiter, $number);
+        }
+        else {
+            $lineJumpPosition = strpos($number, "\n");
+            $inputArguments = substr($number, $lineJumpPosition + 1, (strlen($number)));
+            $inputArguments = preg_split("/[$delimiter]/", $inputArguments);
+        }
+        return $inputArguments;
     }
 }
