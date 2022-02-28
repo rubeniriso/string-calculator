5fc0a8370837974fc9ee0ede45bdac923f644ad2
rubeniriso <rubenirisoe@gmail.com>
2022-02-28 08:49:06 +0100

    [#1] Project uploaded

---
+++ src/StringCalculator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace RubenIriso\StringCalculatorKata;
+
+class StringCalculator
+{
+
+}
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RubenIriso\StringCalculatorKata\Test;
+
+use RubenIriso\StringCalculatorKata\StringCalculator;
+use PHPUnit\Framework\TestCase;
+
+final class StringCalculatorTest extends TestCase
+{
+
+
+}
